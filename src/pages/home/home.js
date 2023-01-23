78cc802db943c4048430403a182bf9450b282114
Jesus Lara <jelara99@gmail.com>
2023-01-23 15:38:39 -0600

    navegacion y vistas

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,16 +1,8 @@
-import { useContext} from "react";
-import { Context } from "../../store/appContext";
-import { Link } from "react-router-dom";
-
 
 export default function Home() {
-  const { store} = useContext(Context);
-  return (
+    return (
     <div className="Principal">
-      <Link to="/demo">
-        Go to Demo
-      </Link>
-      <h1>{store.saludo}</h1>
+      <h1>Home</h1>
     </div>
   )
 }
